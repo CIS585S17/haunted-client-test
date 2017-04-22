fcd34d5ded39d9a3d3cce1229c4fcc01bc0392b3
Gunnarr07 <jeremyrt88@gmail.com>
2017-04-22 10:43:10 -0500

    refactor

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 
-// var socket = require('socket.io-client')('ws://cslinux.cs.ksu.edu:2000');
-let socket = require('socket.io-client')('http://localhost:5000')
+// var socket = require('socket.io-client')('http://cslinux.cs.ksu.edu:5333')
+let socket = require('socket.io-client')('http://localhost:5333')
 socket.on('connect', function(){
   console.log("connected");
-  socket.emit('message', 'foobar');
+  // socket.emit('message', 'foobar');
   // socket.emit('get', 'get the game')
+  socket.emit('host-game', 'game 1')
 });
 socket.on('event', function(data){
   console.log("event", data);
