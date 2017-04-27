77091c8fb8593b69a66a3628cec62adaf728863f
Gunnarr07 <jeremyrt88@gmail.com>
2017-04-27 12:22:05 -0500

    refactor

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
 
 // var socket = require('socket.io-client')('http://cslinux.cs.ksu.edu:5333')
-let socket = require('socket.io-client')('http://localhost:5333')
+let socket = require('socket.io-client')('http://localhost:5000')
+// let socket = require('socket.io-client')('https://haunted-server.herokuapp.com')
 socket.on('connect', function(){
   console.log("connected");
   // socket.emit('message', 'foobar');
   // socket.emit('get', 'get the game')
-  socket.emit('host-game', 'game 1')
+  socket.emit('host-game', 'game 1', (error) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('game 1')
+    }
+  })
 });
 socket.on('event', function(data){
   console.log("event", data);
@@ -14,7 +21,18 @@ socket.on('disconnect', function(){
   console.log("disconnected");
 });
 
+socket.emit('host-game', 'game 1', (error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('game 1')
+  }
+})
 
 socket.on('get-games', (game) => {
   console.log(game)
 })
+
+// socket.on('game-name-error', (msg) => {
+//   console.log(msg)
+// })
